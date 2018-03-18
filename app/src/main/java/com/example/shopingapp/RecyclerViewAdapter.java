dbea2ff2497fe8952fbb3f3537866b9870f701c1
DESKTOP-D8ANFD0\정우현 <korin57@naver.com>
2018-03-18 20:25:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/shopingapp/RecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package com.example.shopingapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by 정우현 on 2017-09-15.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
