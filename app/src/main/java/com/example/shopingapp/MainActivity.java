3d3688e3f4333b3de88318616f1af9d3385030fe
DESKTOP-D8ANFD0\정우현 <korin57@naver.com>
2018-03-18 20:35:22 +0900

    Initial commit

--- app/src/main/java/com/example/shopingapp/MainActivity.java
+++ app/src/main/java/com/example/shopingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shopingapp;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int a = 10;
+        int b = 0;
+        for (int i = 0; i < a; i++)
+        {
+            b+= a+i;
+        }
     }
 }
